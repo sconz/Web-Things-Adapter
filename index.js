5e8e6ff341f4935e945506f4a01596da9da07f3d
sconz <37197720+sconz@users.noreply.github.com>
2018-06-06 13:27:49 +1200

    - add curl example to code

--- index.js
+++ index.js
@@ -4,13 +4,20 @@ var querystring = require("querystring");
 var server = http.createServer();
 var port = process.env.PORT || 3000;
 
+const shell = require('shelljs');
+
 server.on("request", function (request, response) {
   var uri = url.parse(request.url);
   var qs = uri.query ? querystring.parse(uri.query) : {};
 
   var status = qs.status || 200;
   var contentType = qs.contentType || "text/plain";
-  var body = qs.body || "hello there!";
+  var body = qs.body || "IoT device works fine!";
+
+  console.log(qs.iot-passwrod);
+  console.log(qs.iot-name);
+  console.log(qs.iot-property);
+  console.log(qs.iot-command);
 
   response.writeHead(status, {
     "Content-Type": contentType,
@@ -19,6 +26,9 @@ server.on("request", function (request, response) {
 
   console.log(uri.pathname + " - HTTP " + status + " (" + contentType + "): " + body);
   console.log(qs);
+
+  //shell.exec(comandToExecute, {silent:true}).stdout;
+  shell.exec('./get-things.sh'+' '+qs.iot-name+' '+qs.iot-property+' '+iot-command);
 
   response.end(body);
 });
